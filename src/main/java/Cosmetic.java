991220ce29a357ad809c5b02be6a6db069adddb8
aleksandra08052008-cmyk <aleksandra08052008@gmail.com>
2026-05-21 15:26:16 +0400

    Create Cosmetic.java

---
+++ src/main/java/Cosmetic.java
@@ -0,0 +1,63 @@
+import java.util.Objects;
+
+/**
+ * Клас Cosmetic (косметика)
+ */
+public class Cosmetic {
+
+    private String name;
+    private String brand;
+    private double price;
+    private int volume;
+    private int rating;
+
+    public Cosmetic(String name, String brand, double price, int volume, int rating) {
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.volume = volume;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cosmetic)) return false;
+        Cosmetic cosmetic = (Cosmetic) o;
+        return Double.compare(price, cosmetic.price) == 0 &&
+                volume == cosmetic.volume &&
+                rating == cosmetic.rating &&
+                Objects.equals(name, cosmetic.name) &&
+                Objects.equals(brand, cosmetic.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, brand, price, volume, rating);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + brand + " " + price + " " + volume + " " + rating;
+    }
+}
